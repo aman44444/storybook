fd648e4ef8dd054367aa13ca53a40ec96fb7404e
aman1357 <amancorp04@gmail.com>
2025-12-05 11:27:51 +0530

    .

--- src/stories/Otp.stories.ts
+++ src/stories/Otp.stories.ts
@@ -1,15 +1,37 @@
 import type {Meta, StoryObj} from "@storybook/react";
-import Otp from "../components/Otp";
+import Otp from "../components/Otpinput";
 
 const meta : Meta<typeof Otp> = {
-    component : Otp,
-    title : "Ui/Otp",
+    title: "UI/Otp",
+    component: Otp,
+    args: {
+        length: 4,
+    }
 }
 
-export default  meta;
 
-export const FirstComponent : StoryObj<typeof Otp> = {
+export default meta;
+
+type Story = StoryObj<typeof Otp>;
+
+export const Default: Story = {};
+ 
+export const SixDigits : Story = {
     args : {
-         
+        length: 6
+
+    }
+}
+
+
+export const Styled : Story = {
+    args : {
+         backgroundColor: "blue"
+     }
+}
+
+export const ClassName : Story = {
+    args : {
+        className : "flex"
     }
 }
